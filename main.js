aa8d8d7a124399a3743f58ebe285579717b424ce
Ignacio Nicolas Neira <ineira@practia.global>
2021-02-25 00:09:45 -0300

    Agregando scripts

---
+++ main.js
@@ -0,0 +1,17 @@
+function mostrarMayuscula() {
+    var datos = document.getElementById('datos').value;
+    datos = convertir(datos.toLowerCase());
+console.log(datos);
+document.getElementById('resultado').innerHTML = datos;
+}
+
+
+function convertir(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/* hint: convert all to uppercase
+function convertirTodo(string){
+    return string.toUpperCase();
+}
+*/
